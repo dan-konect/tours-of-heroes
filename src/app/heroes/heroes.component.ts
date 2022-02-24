5903851c314fa0c261144bdf752ef8799fe33f82
Daniel Chang <dmagma@hotmail.com>
2022-02-24 13:15:26 -0800

    Complete Display lists Module on Angular Tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
 
+// component contains 3 Metadata properties
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss'],
+  selector: "app-heroes", // the component's CSS element selector
+  templateUrl: "./heroes.component.html", // the location of the component's template file.
+  styleUrls: ["./heroes.component.scss"], // the location of the component's private CSS styles.
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes = HEROES; // set heroes to mock database
+  selectedHero?: Hero; // If selectedHero exists, set interface to Hero
 
   constructor() {}
 
   ngOnInit(): void {}
+  // Event listener => {id: "", name: ""}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
 }
